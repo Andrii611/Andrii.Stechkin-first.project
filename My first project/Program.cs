163e86e0d5e64c8d40d735399a773b2a35943dae
Andrii611 <ffgggfegujd@gmail.com>
2025-10-23 09:53:29 +0300

    Teksta Michaela
    Hochu Yagermeister

---
+++ My first project/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace My_first_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta täht ja vajuta enter");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "a":
+                    Console.WriteLine("Sisestasid tähe a");
+                    break;
+
+                case "e":
+                    Console.WriteLine("Sisestasid tähe e");
+                    break;
+                case "i":
+                    Console.WriteLine("Sisestasid tähe i");
+                    break;
+                default:
+                    Console.WriteLine("Ei ole vokaal");
+                    break;
+            }
+        }
+    }
+}
+    
+
